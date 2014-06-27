f0d6783899f8b39b913d6ec7c6c6c59f3047c7e0
Shuai Zhu <shuai168zhu@gmail.com>
2014-06-27 15:02:38 -0700

    Add Clusters List Table
    Change-Id: I069f32116bf3caea5a5b201acc6b636b21c9eb4c

--- v2/src/app/cluster/clusterlist.js
+++ v2/src/app/cluster/clusterlist.js
@@ -1,12 +1,93 @@
-angular.module('compass.clusterlist', [
+var app = angular.module('compass.clusterlist', [
     'ui.router',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'ngTable'
 ])
 
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('clusterList', {
             url: '/clusterlist',
+            controller: 'ClusterListCtrl',
             templateUrl: 'src/app/cluster/cluster-list.tpl.html'
         });
-})
+}) 
+
+.controller('ClusterListCtrl', function($scope, $filter, ngTableParams) {
+  var data = [
+    {
+      clusterName: "Cluster 1",
+      clusterState: "Deploying",
+      deploymentDetails: "Total:17 | Waiting:15 | Installing:2 | Finished:0 | Failed:0",
+      targetSystem: "OpenStack",
+      dateModified: "03/10/2014 10:50 AM"
+    },
+      {clusterName: "Cluster 2",
+      clusterState: "Failed",
+      deploymentDetails: "Total:34 | Waiting:0 | Installing:0 | Finished:0 | Failed:34",
+      targetSystem: "CentOS",
+      dateModified: "01/04/2014 02:00 PM"
+    },
+      {clusterName: "Cluster 3",
+      clusterState: "Undeployed",
+      deploymentDetails: "Total:23 | Waiting:23 | Installing:0 | Finished:0 | Failed:0",
+      targetSystem: "Hadoop",
+      dateModified: "04/02/2014 12:50 AM"
+    },
+      {clusterName: "Cluster 4",
+      clusterState: "Successful",
+      deploymentDetails: "Total:42 | Waiting:0 | Installing:0 | Finished:42 | Failed:0",
+      targetSystem: "Ceph",
+      dateModified: "06/24/2014 11:18 AM"
+    },
+      {clusterName: "Cluster 5", 
+      clusterState: "Deploying",
+      deploymentDetails: "Total:33 | Waiting:3 | Installing:25 | Finished:5 | Failed:0",
+      targetSystem: "OpenStack",
+      dateModified: "06/22/2014 09:02 AM"
+    },
+      {clusterName: "Cluster 6",
+      clusterState: "Undeployed",
+      deploymentDetails: "Total:77 | Waiting:7 | Installing:4 | Finished:65 | Failed:1",
+      targetSystem: "Ceph",
+      dateModified: "03/29/2014 07:34 AM"
+    },
+      {clusterName: "Cluster 7",
+      clusterState: "Successful",
+      deploymentDetails: "Total:7 | Waiting:0 | Installing:0 | Finished:7 | Failed:0",
+      targetSystem: "CentOS",
+      dateModified: "01/27/2014 12:00 PM"
+    }
+             ];
+
+   //button alerts
+  $scope.alert= function(text) {
+    alert('delete?');
+  }
+ $scope.export= function(text) {
+    alert('export?');
+  }
+
+  $scope.sortShow = false;
+  $scope.sortAsc = false;
+
+  $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                                $filter('orderBy')(data, params.orderBy()) :
+                                data;
+
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
+});
+
+
