bbf31b0263571cf69d8a6d351de227a686c83e60
Chi Zhang <Chi.Zhang1@huawei.com>
2014-09-30 16:27:48 -0700

    requirejs
    Change-Id: I590a9b75c6842f0ece16f89ec31a38035360eb84

--- v2/src/app/user/userprofile.js
+++ v2/src/app/user/userprofile.js
@@ -1,17 +1,18 @@
-var app = angular.module('compass.userProfile', [
-    'ui.router',
-    'ui.bootstrap'
-])
+define(['angular'], function() {
+    var userProfile_module = angular.module('compass.userProfile', [
+        //'ui.router',
+        //'ui.bootstrap'
+    ])
 
-.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('userProfile', {
-            url: '/userProfile',
-            //controller: 'userProfileCtrl',
-            templateUrl: 'src/app/user/user-profile.html',
-            authenticate: true,
-            //resolve: {}
-        });
+    userProfile_module.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('userProfile', {
+                url: '/userProfile',
+                //controller: 'userProfileCtrl',
+                templateUrl: 'src/app/user/user-profile.html',
+                authenticate: true,
+                //resolve: {}
+            });
+    })
 })
-
 //combine usersetting and userprofile into a single file?
