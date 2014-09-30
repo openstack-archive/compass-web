a74e8ab0144c6dae9ae1734c5100e24875022616
Chi Zhang <Chi.Zhang1@huawei.com>
2014-09-30 16:27:48 -0700

    added requirejs
    Change-Id: I590a9b75c6842f0ece16f89ec31a38035360eb84

--- v2/src/app/server/server.js
+++ v2/src/app/server/server.js
@@ -1,121 +1,123 @@
-angular.module('compass.server', [
-    'ui.router',
-    'ui.bootstrap',
-    'compass.charts',
-    'compass.findservers',
-    'ngTable'
-])
+define(['uiRouter', 'uiBootstrap', 'angularTable'], function() {
+    var serverModule = angular.module('compass.server', [
+        'ui.router',
+        'ui.bootstrap',
+        'compass.charts',
+        'compass.findservers',
+        'ngTable'
+    ]);
 
-.config(function config($stateProvider) {
-    $stateProvider
-        .state('serverList', {
-            url: '/serverlist',
-            controller: 'serverCtrl',
-            templateUrl: 'src/app/server/server-list.tpl.html',
-            authenticate: true,
-            resolve: {
-                machinesHostsData: function($q, dataService) {
-                    var deferred = $q.defer();
-                    dataService.getAllMachineHosts().success(function(data) {
-                        deferred.resolve(data);
-                    });
-                    return deferred.promise;
-                }
-            }
-        });
-})
-
-.controller('serverCtrl', function($scope, dataService, $filter, ngTableParams, sortingService, machinesHostsData) {
-    $scope.hideunselected = '';
-    $scope.search = {};
-    $scope.allservers = machinesHostsData;
-    $scope.foundResults = [];
-
-    dataService.getServerColumns().success(function(data) {
-        $scope.server_columns = data.machines_hosts;
-    });
-
-    $scope.tableParams = new ngTableParams({
-        page: 1, // show first page
-        count: $scope.allservers.length + 1 // count per page
-    }, {
-        counts: [], // hide count-per-page box
-        total: $scope.allservers.length, // length of data
-        getData: function($defer, params) {
-            var reverse = false;
-            var orderBy = params.orderBy()[0];
-            var orderBySort = "";
-            var orderByColumn = "";
-
-            if (orderBy) {
-                orderByColumn = orderBy.substring(1);
-                orderBySort = orderBy.substring(0, 1);
-                if (orderBySort == "+") {
-                    reverse = false;
-                } else {
-                    reverse = true;
-                }
-            }
-
-            var orderedData = params.sorting() ?
-                $filter('orderBy')($scope.allservers, function(item) {
-                    if (orderByColumn == "switch_ip") {
-                        return sortingService.ipAddressPre(item.switch_ip);
-                    } else {
-                        return item[orderByColumn];
+    serverModule.config(function config($stateProvider) {
+        $stateProvider
+            .state('serverList', {
+                url: '/serverlist',
+                controller: 'serverCtrl',
+                templateUrl: 'src/app/server/server-list.tpl.html',
+                authenticate: true,
+                resolve: {
+                    machinesHostsData: function($q, dataService) {
+                        var deferred = $q.defer();
+                        dataService.getAllMachineHosts().success(function(data) {
+                            deferred.resolve(data);
+                        });
+                        return deferred.promise;
                     }
-                }, reverse) : $scope.allservers;
-
-            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
+                }
+            });
     });
 
-    $scope.selectAllServers = function(flag) {
-        if (flag) {
-            angular.forEach($scope.allservers, function(sv) {
-                sv.selected = true;
-            })
-        } else {
-            angular.forEach($scope.allservers, function(sv) {
-                sv.selected = false;
-            })
-        }
-    };
+    serverModule.controller('serverCtrl', function($scope, dataService, $filter, ngTableParams, sortingService, machinesHostsData) {
+        $scope.hideunselected = '';
+        $scope.search = {};
+        $scope.allservers = machinesHostsData;
+        $scope.foundResults = [];
 
-    $scope.hideUnselected = function() {
-        if ($scope.hideunselected) {
-            $scope.search.selected = true;
-        } else {
-            delete $scope.search.selected;
-        }
-    };
-
-    $scope.reloadServers = function() {
-        dataService.getAllMachineHosts().success(function(data) {
-            $scope.allservers = data;
-            $scope.tableParams.$params.count = $scope.allservers.length;
-            $scope.tableParams.reload();
+        dataService.getServerColumns().success(function(data) {
+            $scope.server_columns = data.machines_hosts;
         });
-    };
 
-    // add newly found servers at the top if allservers array
-    $scope.$watch('foundResults', function(newResults, oldResults) {
-        if (newResults != oldResults) {
-            for (var i = 0; i < newResults.length; i++) {
-                var sv = $filter('filter')($scope.allservers, newResults[i].mac, true);
-                if (sv.length == 0) {
-                    newResults[i].machine_id = newResults[i].id;
-                    delete newResults[i]['id'];
-                    newResults[i].new = true;
-                    $scope.allservers.unshift(newResults[i]);
+        $scope.tableParams = new ngTableParams({
+            page: 1, // show first page
+            count: $scope.allservers.length + 1 // count per page
+        }, {
+            counts: [], // hide count-per-page box
+            total: $scope.allservers.length, // length of data
+            getData: function($defer, params) {
+                var reverse = false;
+                var orderBy = params.orderBy()[0];
+                var orderBySort = "";
+                var orderByColumn = "";
+
+                if (orderBy) {
+                    orderByColumn = orderBy.substring(1);
+                    orderBySort = orderBy.substring(0, 1);
+                    if (orderBySort == "+") {
+                        reverse = false;
+                    } else {
+                        reverse = true;
+                    }
                 }
-            }
 
-            if ($scope.tableParams) {
+                var orderedData = params.sorting() ?
+                    $filter('orderBy')($scope.allservers, function(item) {
+                        if (orderByColumn == "switch_ip") {
+                            return sortingService.ipAddressPre(item.switch_ip);
+                        } else {
+                            return item[orderByColumn];
+                        }
+                    }, reverse) : $scope.allservers;
+
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
+        $scope.selectAllServers = function(flag) {
+            if (flag) {
+                angular.forEach($scope.allservers, function(sv) {
+                    sv.selected = true;
+                })
+            } else {
+                angular.forEach($scope.allservers, function(sv) {
+                    sv.selected = false;
+                })
+            }
+        };
+
+        $scope.hideUnselected = function() {
+            if ($scope.hideunselected) {
+                $scope.search.selected = true;
+            } else {
+                delete $scope.search.selected;
+            }
+        };
+
+        $scope.reloadServers = function() {
+            dataService.getAllMachineHosts().success(function(data) {
+                $scope.allservers = data;
                 $scope.tableParams.$params.count = $scope.allservers.length;
                 $scope.tableParams.reload();
-            }
-        }
-    }, true);
+            });
+        };
 
-})
+        // add newly found servers at the top if allservers array
+        $scope.$watch('foundResults', function(newResults, oldResults) {
+            if (newResults != oldResults) {
+                for (var i = 0; i < newResults.length; i++) {
+                    var sv = $filter('filter')($scope.allservers, newResults[i].mac, true);
+                    if (sv.length == 0) {
+                        newResults[i].machine_id = newResults[i].id;
+                        delete newResults[i]['id'];
+                        newResults[i].new = true;
+                        $scope.allservers.unshift(newResults[i]);
+                    }
+                }
+
+                if ($scope.tableParams) {
+                    $scope.tableParams.$params.count = $scope.allservers.length;
+                    $scope.tableParams.reload();
+                }
+            }
+        }, true);
+
+    });
+});
--- v2/src/common/findservers/findservers.js
+++ v2/src/common/findservers/findservers.js
@@ -1,201 +1,203 @@
-angular.module('compass.findservers', [])
-.directive('switchrow', function(dataService, $timeout) {
-    return {
-        restrict: 'A',
-        scope: {
-            polling: '=',
-            switchinfo: '=',
-            result: '=',
-            machines: '='
-        },
-        link: function(scope, element, attrs) {
-            var checkSwitchTimer;
-            var checkSwitchCount = 0;
-            //var pollingTriggered = scope.polling;
-            var fireTimer = true;
+define(['angular'], function() {
+    var findserversModule = angular.module('compass.findservers', []);
+    findserversModule.directive('switchrow', function(dataService, $timeout) {
+        return {
+            restrict: 'A',
+            scope: {
+                polling: '=',
+                switchinfo: '=',
+                result: '=',
+                machines: '='
+            },
+            link: function(scope, element, attrs) {
+                var checkSwitchTimer;
+                var checkSwitchCount = 0;
+                //var pollingTriggered = scope.polling;
+                var fireTimer = true;
 
-            var getMachines = function() {
-                dataService.getSwitchMachines(scope.switchinfo.id).success(function(data) {
-                    scope.polling = false;
-                    scope.result = "success";
-                    scope.machines = data;
-                }).error(function(data) {
-                    scope.polling = false;
-                    scope.result = "error";
-                })
-            };
-
-            // check switch state 15 times with the interval of 2 sec
-            var checkSwitchState = function() {
-                checkSwitchCount++;
-                dataService.getSwitchById(scope.switchinfo.id).success(function(data) {
-                    if (data.state == "under_monitoring") {
-                        getMachines();
-                    } else if (data.state === "initialized" || data.state === "repolling")
-                        if (fireTimer && checkSwitchCount < 15) {
-                            checkSwitchTimer = $timeout(checkSwitchState, 2000);
-                        } else {
-                            scope.polling = false;
-                            scope.result = "error";
-                        } else {
+                var getMachines = function() {
+                    dataService.getSwitchMachines(scope.switchinfo.id).success(function(data) {
+                        scope.polling = false;
+                        scope.result = "success";
+                        scope.machines = data;
+                    }).error(function(data) {
                         scope.polling = false;
                         scope.result = "error";
-                    }
-                })
-            };
+                    })
+                };
 
-            scope.$watch('polling', function(newval, oldval) {
-                if (newval != oldval) {
-                    if (newval == true) {
-                        checkSwitchCount = 0;
-                        fireTimer = true;
+                // check switch state 15 times with the interval of 2 sec
+                var checkSwitchState = function() {
+                    checkSwitchCount++;
+                    dataService.getSwitchById(scope.switchinfo.id).success(function(data) {
+                        if (data.state == "under_monitoring") {
+                            getMachines();
+                        } else if (data.state === "initialized" || data.state === "repolling")
+                            if (fireTimer && checkSwitchCount < 15) {
+                                checkSwitchTimer = $timeout(checkSwitchState, 2000);
+                            } else {
+                                scope.polling = false;
+                                scope.result = "error";
+                            } else {
+                            scope.polling = false;
+                            scope.result = "error";
+                        }
+                    })
+                };
 
-                        var findingAction = {
-                            "find_machines": null
-                        };
-                        dataService.postSwitchAction(scope.switchinfo.id, findingAction).success(function(data) {
-                            checkSwitchState();
-                        })
-                    }
-                }
-            })
+                scope.$watch('polling', function(newval, oldval) {
+                    if (newval != oldval) {
+                        if (newval == true) {
+                            checkSwitchCount = 0;
+                            fireTimer = true;
 
-            element.bind('$destroy', function() {
-                fireTimer = false;
-                $timeout.cancel(checkSwitchTimer);
-            });
-        }
-    }
-})
-
-.directive('findservers', function(dataService, $modal, $log) {
-    return {
-        restrict: 'E',
-        scope: {
-            newFoundServers: '=results'
-        },
-        templateUrl: "src/common/findservers/find-new-servers.tpl.html",
-        link: function(scope, element, attrs) {
-            scope.switches = [];
-            scope.newFoundServers = [];
-            scope.isFindingNewServers = false;
-
-            dataService.getSwitches().success(function(data) {
-                scope.switches = data;
-            });
-
-            scope.modifySwitchModal = function(index) {
-                var modalInstance = $modal.open({
-                    templateUrl: 'modifySwitchModal.html',
-                    controller: modifySwitchModalInstanceCtrl,
-                    resolve: {
-                        targetSwitch: function() {
-                            return scope.switches[index];
+                            var findingAction = {
+                                "find_machines": null
+                            };
+                            dataService.postSwitchAction(scope.switchinfo.id, findingAction).success(function(data) {
+                                checkSwitchState();
+                            })
                         }
                     }
+                })
+
+                element.bind('$destroy', function() {
+                    fireTimer = false;
+                    $timeout.cancel(checkSwitchTimer);
+                });
+            }
+        }
+    });
+
+    findserversModule.directive('findservers', function(dataService, $modal, $log) {
+        return {
+            restrict: 'E',
+            scope: {
+                newFoundServers: '=results'
+            },
+            templateUrl: "src/common/findservers/find-new-servers.tpl.html",
+            link: function(scope, element, attrs) {
+                scope.switches = [];
+                scope.newFoundServers = [];
+                scope.isFindingNewServers = false;
+
+                dataService.getSwitches().success(function(data) {
+                    scope.switches = data;
                 });
 
-                modalInstance.result.then(function(targetSwitch) {
-                    scope.alerts = [];
-                    dataService.putSwitches(targetSwitch.id,targetSwitch).success(function() {
+                scope.modifySwitchModal = function(index) {
+                    var modalInstance = $modal.open({
+                        templateUrl: 'modifySwitchModal.html',
+                        controller: modifySwitchModalInstanceCtrl,
+                        resolve: {
+                            targetSwitch: function() {
+                                return scope.switches[index];
+                            }
+                        }
+                    });
 
-                        scope.switches[index] = angular.copy(targetSwitch);
+                    modalInstance.result.then(function(targetSwitch) {
+                        scope.alerts = [];
+                        dataService.putSwitches(targetSwitch.id, targetSwitch).success(function() {
+
+                            scope.switches[index] = angular.copy(targetSwitch);
+
+                        }).error(function(response) {
+                            scope.alerts[0] = response;
+                        });
+                    }, function() {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    });
+                };
+
+                scope.selectAllSwitches = function(flag) {
+                    if (flag) {
+                        angular.forEach(scope.switches, function(sv) {
+                            sv.selected = true;
+                        })
+                    } else {
+                        angular.forEach(scope.switches, function(sv) {
+                            sv.selected = false;
+                        })
+                    }
+                };
+
+                scope.findServers = function() {
+                    var swSelection = false;
+                    angular.forEach(scope.switches, function(sw) {
+                        if (sw.selected) {
+                            swSelection = true;
+                        }
+                    });
+                    if (!swSelection) {
+                        alert("Please select at least one switch");
+                    } else {
+                        scope.isFindingNewServers = true;
+                        scope.newFoundServers = [];
+                        angular.forEach(scope.switches, function(sw) {
+                            if (sw.selected) {
+                                sw.result = "";
+                                sw.finished = false;
+                                sw.polling = true;
+                            }
+                        });
+                    }
+                };
+
+                scope.$watch('switches', function(val) {
+                    var totalResultReady = true;
+                    if (scope.isFindingNewServers) {
+                        angular.forEach(scope.switches, function(sw) {
+                            if (sw.selected && !sw.finished) {
+                                if (sw.result == "success") {
+                                    scope.newFoundServers = scope.newFoundServers.concat(angular.copy(sw.machines));
+                                    sw.finished = true;
+                                } else if (sw.result == "error") {
+                                    sw.finished = true;
+                                } else {
+                                    totalResultReady = false;
+                                }
+                            }
+                        });
+                        if (totalResultReady) {
+                            scope.isFindingNewServers = false;
+                        }
+                    }
+                }, true);
+
+
+                scope.newswitch = {}
+                scope.newswitch.credentials = {};
+                scope.alerts = [];
+
+                scope.addSwitch = function() {
+                    scope.alerts = [];
+                    dataService.postSwitches(scope.newswitch).success(function(switchData) {
+
+                        scope.newswitch = {};
+
+
+                        scope.switches.push(switchData);
 
                     }).error(function(response) {
                         scope.alerts[0] = response;
                     });
-                }, function() {
-                    $log.info('Modal dismissed at: ' + new Date());
-                });
-            };
+                };
 
-            scope.selectAllSwitches = function(flag) {
-                if (flag) {
-                    angular.forEach(scope.switches, function(sv) {
-                        sv.selected = true;
-                    })
-                } else {
-                    angular.forEach(scope.switches, function(sv) {
-                        sv.selected = false;
-                    })
-                }
-            };
+                scope.closeAlert = function() {
+                    scope.alerts = [];
+                };
+            }
+        };
+    });
+    var modifySwitchModalInstanceCtrl = function($scope, $modalInstance, targetSwitch) {
+        $scope.targetSwitch = angular.copy(targetSwitch);
+        $scope.ok = function() {
+            $modalInstance.close($scope.targetSwitch);
+        };
 
-            scope.findServers = function() {
-                var swSelection = false;
-                angular.forEach(scope.switches, function(sw) {
-                    if (sw.selected) {
-                        swSelection = true;
-                    }
-                });
-                if (!swSelection) {
-                    alert("Please select at least one switch");
-                } else {
-                    scope.isFindingNewServers = true;
-                    scope.newFoundServers = [];
-                    angular.forEach(scope.switches, function(sw) {
-                        if (sw.selected) {
-                            sw.result = "";
-                            sw.finished = false;
-                            sw.polling = true;
-                        }
-                    });
-                }
-            };
-
-            scope.$watch('switches', function(val) {
-                var totalResultReady = true;
-                if (scope.isFindingNewServers) {
-                    angular.forEach(scope.switches, function(sw) {
-                        if (sw.selected && !sw.finished) {
-                            if (sw.result == "success") {
-                                scope.newFoundServers = scope.newFoundServers.concat(angular.copy(sw.machines));
-                                sw.finished = true;
-                            } else if (sw.result == "error") {
-                                sw.finished = true;
-                            } else {
-                                totalResultReady = false;
-                            }
-                        }
-                    });
-                    if (totalResultReady) {
-                        scope.isFindingNewServers = false;
-                    }
-                }
-            }, true);
-
-
-            scope.newswitch = {}
-            scope.newswitch.credentials = {};
-            scope.alerts = [];
-
-            scope.addSwitch = function() {
-                scope.alerts = [];
-                dataService.postSwitches(scope.newswitch).success(function(switchData) {
-
-                    scope.newswitch = {};
-
-
-                    scope.switches.push(switchData);
-
-                }).error(function(response) {
-                    scope.alerts[0] = response;
-                });
-            };
-
-            scope.closeAlert = function() {
-                scope.alerts = [];
-            };
-        }
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        };
     };
-})
-var modifySwitchModalInstanceCtrl = function ($scope, $modalInstance,targetSwitch) {
-  $scope.targetSwitch = angular.copy(targetSwitch);
-  $scope.ok = function () {
-    $modalInstance.close($scope.targetSwitch);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-};
+});
