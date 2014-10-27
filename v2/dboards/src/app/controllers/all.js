389fdf146be8330451f1c51b020a17709feec495
GitaWei <laurenzheda@gmail.com>
2014-10-26 21:32:19 -0700

    Added source for the dashboard with Kairosdb ext
    Change-Id: Ifebd657560f685e3361a7334994fd8d590812acc

---
+++ v2/dboards/src/app/controllers/all.js
@@ -0,0 +1,16 @@
+define([
+  './dash',
+  './dashLoader',
+  './row',
+  './submenuCtrl',
+  './pulldown',
+  './search',
+  './metricKeys',
+  './graphiteTarget',
+  './graphiteImport',
+  './influxTargetCtrl',
+  './playlistCtrl',
+  './inspectCtrl',
+  './opentsdbTargetCtrl',
+  './kairosdbTargetCtrl',
+], function () {});
